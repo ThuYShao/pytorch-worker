a121e6431f93de335ae5a8a6cb6975a35dde3fd9
THUYShao <shaoyunqiu14@gmail.com>
2019-11-13 14:29:10 +0900

    fix output

--- model/nlp/BertPoolOutMax.py
+++ model/nlp/BertPoolOutMax.py
@@ -22,63 +22,22 @@ class BertPoolOutMax(nn.Module):
 
     def forward(self, data, config, gpu_list, acc_result, mode):
         input_ids, attention_mask, token_type_ids = data['input_ids'], data['attention_mask'], data['token_type_ids']
-        # print(input_ids.size(), attention_mask.size(), token_type_ids.size())
-        # batch_size = input_ids.size()[0]
-        with torch.no_grad():
-            # input_ids = input_ids.view(-1, self.max_len)
-            # attention_mask = attention_mask.view(-1, self.max_len)
-            # token_type_ids = token_type_ids.view(-1, self.max_len)
 
+        with torch.no_grad():
+            output = []
             for k in range(input_ids.size()[0]):
                 q_lst = []
                 for i in range(self.max_para_q):
-                    input_ids_row = input_ids[k, i].view(-1, self.max_len)
-                    attention_mask_row = attention_mask[k, i].view(-1, self.max_len)
-                    token_type_ids_row = token_type_ids[k, i].view(-1, self.max_len)
-                    #print(input_ids_row.size(), attention_mask_row.size(), token_type_ids_row.size())
-                    _, lst = self.bert(input_ids_row, token_type_ids=token_type_ids_row,
-                                       attention_mask=attention_mask_row)
-                    # lst = []
-                    # for j in range(self.max_para_c):
-                    #     _, y = self.bert(input_ids[k, i, j].unsqueeze(0), token_type_ids=token_type_ids[k, i, j].unsqueeze(0),
-                    #                      attention_mask=attention_mask[k, i, j].unsqueeze(0))
-                    #     y = y.view(1, -1)
-                    #     lst.append(y)
-                    # lst = torch.cat(lst, dim=0)
-                    # print('after concat', lst.size())
+                    _, lst = self.bert(input_ids[k, i].view(-1, self.max_len),
+                                       token_type_ids=token_type_ids[k, i].view(-1, self.max_len),
+                                       attention_mask=attention_mask[k, i].view(-1, self.max_len))
                     lst = lst.view(self.max_para_c, -1)
                     lst = lst.transpose(0, 1)
-                    #print('after transpose', lst.size())
                     lst = lst.unsqueeze(0)
-                    #print('after unsqueeze', lst.size())
                     max_out = self.maxpool(lst)
                     max_out = max_out.squeeze()
-                    #print('max out size', max_out.size())
                     q_lst.append(max_out.detach().cpu().tolist())
-                    #input('continue?')
-                print(len(q_lst))
-            input('continue to print content of q_list?')
-            print(q_lst[0])
-
-
-            # _, y = self.bert(input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask,
-            #                  output_all_encoded_layers=False)
-            #
-            # y = y.view(-1, self.max_para_q, self.max_para_c, 768)
-            #
-            # y = y.permute(0, 3, 1, 2)
-            #
-            # y = self.maxpool(y)
-            #
-            # y = y.view(-1, 768, self.max_para_q, 1)
-            #
-            # y = y.squeeze(3)
-            #
-            # y = y.permute(0, 2, 1)
-
-            # output = []
-            # y = y.cpu().detach().numpy().tolist()
-            # for i, guid in enumerate(data['guid']):
-            #     output.append([guid, y[i]])
-            # return {"output": output}
+                assert (len(q_lst) == self.max_para_q)
+                output.append([data['guid'][k], q_lst])
+            return {"output": output}
         
