ae8023dcb5172d8fe987393051397436254c50bd
THUYShao <shaoyunqiu14@gmail.com>
2019-12-28 16:30:25 +0800

    fix ephoch select output

--- my_utils/epoch_select.py
+++ my_utils/epoch_select.py
@@ -21,7 +21,9 @@ def select_best_epoch(_eval_dict, _key):
     for type_ in _eval_dict:
         tmp_dict = _eval_dict[type_]
         sort_lst = sorted(tmp_dict.items(), key=lambda item: item[1][_key], reverse=True)
-        print('type=%s, epoch=%d' % (type_, int(sort_lst[0][0])), sort_lst[0][1])
+        print('type=%s, epoch=%d' % (type_, int(sort_lst[0][0])))
+        for p_t in _eval_dict:
+            print(p_t, _eval_dict[p_t][sort_lst[0][0]])
 
 
 def early_stop(_eval_dict, _key, _th=10):
@@ -34,27 +36,35 @@ def early_stop(_eval_dict, _key, _th=10):
     if _key != 'loss':
         tmp_best = -1
         best_epoch = -1
-        for item in sort_lst:
+        tmp_idx = -1
+        for idx, item in enumerate(sort_lst):
             epoch_idx = int(item[0])
             score = item[1][_key]
             if score > tmp_best:
                 tmp_best = score
                 best_epoch = epoch_idx
+                tmp_idx = idx
             if epoch_idx - best_epoch > _th:
                 break
-        print('epoch=%d, best_score=%f' % (best_epoch, tmp_best), sort_lst[best_epoch])
+        print('epoch=%d, best_score=%f' % (best_epoch, tmp_best), sort_lst[tmp_idx])
+        if 'test' in _eval_dict:
+            print('on test set', _eval_dict['test']['%d' % best_epoch])
     else:
         tmp_best = 1000
         best_epoch = -1
-        for item in sort_lst:
+        tmp_idx = -1
+        for idx, item in enumerate(sort_lst):
             epoch_idx = int(item[0])
             score = item[1][_key]
             if score < tmp_best:
                 tmp_best = score
                 best_epoch = epoch_idx
+                tmp_idx = idx
             if epoch_idx - best_epoch > _th:
                 break
-        print('epoch=%d, best_score=%f' % (best_epoch, tmp_best), sort_lst[best_epoch])
+        print('epoch=%d, best_score=%f' % (best_epoch, tmp_best), sort_lst[tmp_idx])
+        if 'test' in _eval_dict:
+            print('on test set', _eval_dict['test']['%d' % best_epoch])
 
 
 if __name__ == '__main__':
